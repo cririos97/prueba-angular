ebfbd4ba04fdc1d3a68d6207f58b07abe27770ad
cririos <cririos@outlook.com>
2019-06-18 20:45:17 -0500

    listar usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MenuComponent,
-    NavbarComponent
+    NavbarComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
